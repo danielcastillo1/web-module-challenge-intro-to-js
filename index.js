a83e8db1ea6afa2e77f3525b325845f410891324
Daniel Castillo <danielcastillo89@icloud.com>
2023-02-01 23:13:24 -0600

    completed module 2 sprint

--- index.js
+++ index.js
@@ -61,9 +61,11 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(num1, num2){
-  return num1 * num2;
+function multiply(a, b){
+  return a * b;
 }
+ console.log("task 1d", multiply(4, 6));
+
 
 
 
@@ -77,11 +79,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7;
 }
 
-
+console.log("task 2" , dogYears(10));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -129,11 +131,25 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if( age >= 1 && weight <= 5 ) {
+    return weight * 0.05;
+  }else if (age >= 1 && weight >= 6 && weight <= 10 ) {
+return weight * 0.04;
+  } else if(age >= 1 && weight >= 11 && weight <= 15 ){
+    return weight * 0.03;
+  } else if (age >= 1 && weight > 15) {
+    return weight * 0.02;
+  } else if (age < 1 && age >= .58 ){
+    return weight * 0.04;
+  } else if (age <= .58 && age >= .33 ){
+    return weight * 0.05;
+  } else if (age < .33) {
+    return weight * 0.10;
+  }
 }
 
-
+console.log("task 3", hungryDog(2, 10));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -155,9 +171,26 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
-
+let randomChoice = Math.random();
+if(randomChoice <= 0.34) {
+  randomChoice = "rock" ;
+} else if (randomChoice <= 0.67){
+  randomChoice = "paper" ;
+}else if (randomChoice > 0.67) {
+  randomChoice = "scissors"
+}
 function game(user, computer){
-  /*add your code here*/
+  if(user === computer) {
+    return `it's a tie`;
+  } else if(user === "rock" && computer === "scissors" ){
+    return `you win!`;
+  } else if (user === "paper" && computer === "rock" ) {
+    return `you win!`;
+  } else if (user === "scissors" && computer === "paper") {
+    return `you win!`;
+  } else {
+    return `you lose!`;
+  }
 }
 
 
@@ -173,11 +206,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
 
-
+console.log("task 5", miles(8));
 
 //Task 5b - Centimeters to Feet
 /*
@@ -187,11 +220,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48
 }
 
-
+console.log("task 5b", feet(100));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -207,10 +240,12 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+    for(let i = number; i > 0; i--){
+      return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i -1} bottles of soda on the wall`;
+    }
 }
-
+console.log("task 6", annoyingSong(12));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -227,11 +262,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+if(score >= 90){
+  return `you got an A`;
+} else if(score < 90 && score >= 80 ){
+  return `you got a B`;
+} else if(score < 80 && score >= 70){
+  return `you got a C`;
+} else if(score < 70 && score >= 60){
+  return `you got a D`;
+} else if( score < 60){
+  return `you got an F`;
+}
 }
 
-
+console.log("task 7", grade(70));
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
